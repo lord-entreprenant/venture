25e5124caeb8f46e158bacf2f90f621eec8b25fd
Lord Entreprenant <lord-entreprenant@users.noreply.github.com>
2015-02-27 20:58:58 +0530

    Create ENTERPRISE.java

---
+++ prod/ENTERPRISE.java
@@ -0,0 +1,12 @@
+/**
+ * File Info.
+ * Name: ENTERPRISE.java
+ * Author: Lord Entreprenant
+ * Version: 0.0.1
+ */
+
+public class ENTERPRISE {
+  public static void main(String[] args) {
+  }
+}
+// EOF
